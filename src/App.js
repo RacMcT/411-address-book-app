6531597d05875863799e8931d8701db1510c05cb
RacMcT <raccmct@gmail.com>
2021-07-22 14:28:29 -0500

    connected to api for user data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+// resource for syntax for displaying first and last name on same column in react!!
+// https://stackoverflow.com/questions/57321083/displaying-firstname-and-last-name-in-the-same-column-in-react-table-2
+
 function App() {
+  let [users, setUsers] = useState([]);
+
+  let fetchData = () => {
+    fetch('https://randomuser.me/api?results=25')
+      .then(res => res.json())
+      .then(data => data.results.map(user => (
+        {
+          name: '${user.name.first} ${user.name.last}',
+          phone: user.phone,
+          email: user.email,
+          pic: user.picture.thumbnail
+        }
+      )))
+      .then(contacts => setUsers(contacts))
+      // .catch(error => console.log('errr', error))-- do I need to add an error message?- ask Matt since was in the whiteboarding tests...
+  
+    };
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+      <ul>
+       
+      </ul>
     </div>
   );
 }
